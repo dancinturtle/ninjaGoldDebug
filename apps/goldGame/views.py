6a09a424eae4b61dd9bd05a3352f2b6c9c2d602d
Cesar Kyle Casil <ccasil@ucsc.edu>
2018-02-20 20:08:40 -0800

    solved all bugs

--- apps/goldGame/views.py
+++ apps/goldGame/views.py
@@ -1,40 +1,36 @@
 from django.shortcuts import render, redirect
 import random, datetime
-from time import gmtime, strftime
 
 def index(request):
     if 'totalgold' not in request.session:
         request.session['totalgold'] = [0]
         request.session['journal'] = []
+    
+    context = {"total":request.session['totalgold'][-1]}
 
-    context = {
-        "total":request.session['totalgold'][-1]
-    }
-    print total
     return render(request, "goldGame/index.html", context)
 
 def process(request):
-    time = strftime("%Y-%m-%d %I:%M %p", gmtime())
-
-    buildings = {
-        'farm': [10,20], 
-        'cave': [5,10], 
-        'house':[2,5], 
-        'casino':[-50, 50]
-        }
-
+    time = datetime.datetime.now().strftime("%Y-%m-%d %I:%M %p")
+    buildings = {'farm': [10,20], 'cave': [5,10], 'house':[2,5], 'casino':[-50, 50]}
     if request.method == "POST":
+
         building = request.POST['building']
+
         randomNum = random.randint(buildings[building][0], buildings[building][1])
+        
         request.session['totalgold'].append(request.session['totalgold'][-1] + randomNum)
 
         if(randomNum < 0):
             request.session['journal'].append("Entered a casino and lost {} gold...Ouch. {}".format(-randomNum, time))
+    
         else :
             request.session['journal'].append("Earned {} gold from the {}! {}".format(randomNum, building, time))
+        
+        request.session.modified = True
+
     return redirect('/')
 
 def reset(request):
-    request.session['totalgold'].clear()
-    request.session['journal'].clear()
+    request.session.clear()
     return redirect('/')
