4f16c24829ddbf4615d1fccea179225e0f982f05
Amenda <amendahing@gmail.com>
2018-02-21 10:34:26 -0800

    debugged 4 errors

--- apps/goldGame/views.py
+++ apps/goldGame/views.py
@@ -5,31 +5,32 @@ def index(request):
     if 'totalgold' not in request.session:
         request.session['totalgold'] = [0]
         request.session['journal'] = []
-    
+    print request.session['totalgold']
     context = {"total":request.session['totalgold'][-1]}
-
-    return render("goldGame/index.html", context)
+    # return render(request, "goldGame/index.html")
+    return render(request, "goldGame/index.html", context)
 
 def process(request):
     time = datetime.datetime.now().strftime("%Y-%m-%d %I:%M %p")
     buildings = {'farm': [10,20], 'cave': [5,10], 'house':[2,5], 'casino':[-50, 50]}
+
     if request.method == "POST":
-
-        building = request.POST['Building']
-
+        building = request.POST['building']
         randomNum = random.randint(buildings[building][0], buildings[building][1])
-        
+
         request.session['totalgold'].append(request.session['totalgold'][-1] + randomNum)
 
         if(randomNum < 0):
             request.session['journal'].append("Entered a casino and lost {} gold...Ouch. {}".format(-randomNum, time))
-    
+            return redirect('/')
+
         else :
             request.session['journal'].append("Earned {} gold from the {}! {}".format(randomNum, building, time))
-    
+            return redirect('/')
+
     return redirect('/')
 
 def reset(request):
-    request.session['totalgold'].clear()
-    request.session['journal'].clear()
+    request.session['totalgold'] = [0]
+    request.session['journal'] = [0]
     return redirect('/')
