460ee52af13faba7157b067b7655dd0957610a42
thinhle93 <thinhle93@gmail.com>
2018-02-20 16:20:36 -0800

    ninjagold debug

--- apps/goldGame/views.py
+++ apps/goldGame/views.py
@@ -1,28 +1,30 @@
 from django.shortcuts import render, redirect
-import random, datetime
+import random
+from datetime import datetime
+from time import strftime
 
 def index(request):
     if 'totalgold' not in request.session:
-        request.session['totalgold'] = [0]
+        request.session['totalgold'] = 0
         request.session['journal'] = []
     
-    context = {"total":request.session['totalgold'][-1]}
+    context = {"total":request.session['totalgold']}
 
-    return render("goldGame/index.html", context)
+    return render(request, "goldGame/index.html", context)
 
 def process(request):
-    time = datetime.datetime.now().strftime("%Y-%m-%d %I:%M %p")
+    time = datetime.now().strftime("%Y-%m-%d %I:%M %p")
     buildings = {'farm': [10,20], 'cave': [5,10], 'house':[2,5], 'casino':[-50, 50]}
     if request.method == "POST":
 
-        building = request.POST['Building']
+        building = request.POST['building']
 
         randomNum = random.randint(buildings[building][0], buildings[building][1])
         
-        request.session['totalgold'].append(request.session['totalgold'][-1] + randomNum)
-
+        request.session['totalgold'] = request.session['totalgold'] + randomNum
+        print request.session['totalgold']  
         if(randomNum < 0):
-            request.session['journal'].append("Entered a casino and lost {} gold...Ouch. {}".format(-randomNum, time))
+            request.session['journal'].append("Entered a casino and lost {} gold...Ouch. {}".format(randomNum, time))
     
         else :
             request.session['journal'].append("Earned {} gold from the {}! {}".format(randomNum, building, time))
@@ -30,6 +32,6 @@ def process(request):
     return redirect('/')
 
 def reset(request):
-    request.session['totalgold'].clear()
-    request.session['journal'].clear()
+    request.session['totalgold'] = 0
+    request.session['journal']= []
     return redirect('/')
