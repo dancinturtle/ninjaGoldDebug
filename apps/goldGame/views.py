375a8af696a17065c5ab0d888c2dd03cd0bee4d8
Jonathan Yee <Jonathan.Yee.W@gmail.com>
2018-02-20 17:02:10 -0800

    Jonathan Yee

--- apps/goldGame/views.py
+++ apps/goldGame/views.py
@@ -8,28 +8,40 @@ def index(request):
     
     context = {"total":request.session['totalgold'][-1]}
 
-    return render("goldGame/index.html", context)
+    return render(request, "goldGame/index.html", context) #error added request
 
-def process(request):
+def process_money(request): #error process needs to match process_money
     time = datetime.datetime.now().strftime("%Y-%m-%d %I:%M %p")
     buildings = {'farm': [10,20], 'cave': [5,10], 'house':[2,5], 'casino':[-50, 50]}
     if request.method == "POST":
-
-        building = request.POST['Building']
-
-        randomNum = random.randint(buildings[building][0], buildings[building][1])
         
-        request.session['totalgold'].append(request.session['totalgold'][-1] + randomNum)
+        building = request.POST['building']
+        print building
+        randomNum = random.randint(buildings[building][0], buildings[building][1])
+        print randomNum
+        print request.session['totalgold']
+        temporaryTotalGold = request.session['totalgold'][0]
+        print temporaryTotalGold
+        temporaryTotalGold += randomNum
+        print temporaryTotalGold
+        print int(temporaryTotalGold)
 
-        if(randomNum < 0):
-            request.session['journal'].append("Entered a casino and lost {} gold...Ouch. {}".format(-randomNum, time))
+        # request.session['totalgold'] = int(temporaryTotalGold)
+
+        # request.session['totalgold'] = temporaryTotalGold
+
+        # request.session['totalgold'].append(request.session['totalgold'][-1] + randomNum)
+        # print request.session['totalgold']
+
+        # if(randomNum < 0):
+        #     request.session['journal'].append("Entered a casino and lost {} gold...Ouch. {}".format(-randomNum, time))
     
-        else :
-            request.session['journal'].append("Earned {} gold from the {}! {}".format(randomNum, building, time))
+        # else :
+        #     request.session['journal'].append("Earned {} gold from the {}! {}".format(randomNum, building, time))
     
     return redirect('/')
 
 def reset(request):
-    request.session['totalgold'].clear()
-    request.session['journal'].clear()
+    request.session['totalgold'] = [0] #error clear wasn't working
+    request.session['journal'] = [] #error clear wasn't working "object has no attribute 'upper'"
     return redirect('/')
